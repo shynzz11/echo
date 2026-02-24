94d7b8f83d10f72cd11e1ed93f4fef73724604a9
Shayan <shay.dey011@gmail.com>
2026-02-24 20:35:37 +0530

    19: ai tool calling

--- packages/backend/convex/private/conversations.ts
+++ packages/backend/convex/private/conversations.ts
@@ -1,9 +1,59 @@
-import { query } from "../_generated/server"
+import { mutation, query } from "../_generated/server"
 import { ConvexError, v } from "convex/values"
 import { supportAgent } from "../system/ai/agents/supportAgent"
 import { MessageDoc, saveMessage } from "@convex-dev/agent"
 import { paginationOptsValidator, PaginationResult } from "convex/server"
 import { Doc } from "../_generated/dataModel"
+
+export const updateStatus = mutation({
+	args: {
+		conversationId: v.id("conversations"),
+		status: v.union(
+			v.literal("unresolved"),
+			v.literal("escalated"),
+			v.literal("resolved"),
+		),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity()
+
+		if (identity === null) {
+			throw new ConvexError({
+				code: "UNAUTHORIZED",
+				message: "Identity not found",
+			})
+		}
+
+		const orgId = identity.orgId as string
+
+		if (!orgId) {
+			throw new ConvexError({
+				code: "UNAUTHORIZED",
+				message: "Organization not found",
+			})
+		}
+
+		const conversation = await ctx.db.get(args.conversationId)
+
+		if (!conversation) {
+			throw new ConvexError({
+				code: "NOT_FOUND",
+				message: "Conversation not found",
+			})
+		}
+
+		if (conversation.organizationId !== orgId) {
+			throw new ConvexError({
+				code: "UNAUTHORIZED",
+				message: "Invalid Organization Id",
+			})
+		}
+
+		await ctx.db.patch(args.conversationId, {
+			status: args.status,
+		})
+	},
+})
 
 export const getOne = query({
 	args: {
--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -1,8 +1,11 @@
 import { ConvexError, v } from "convex/values"
 import { action, query } from "../_generated/server"
-import { internal } from "../_generated/api"
+import { components, internal } from "../_generated/api"
 import { supportAgent } from "../system/ai/agents/supportAgent"
 import { paginationOptsValidator } from "convex/server"
+import { escalateConversation } from "../system/ai/tools/escalateConversation"
+import { resolveConversation } from "../system/ai/tools/resolveConversation"
+import { saveMessage } from "@convex-dev/agent"
 
 export const create = action({
 	args: {
@@ -47,16 +50,28 @@ export const create = action({
 		}
 
 		//TODO: implement subscription check
+		const shouldTriggerAgent = conversation.status === "unresolved"
 
-		await supportAgent.generateText(
-			ctx,
-			{
+		if (shouldTriggerAgent) {
+			await supportAgent.generateText(
+				ctx,
+				{
+					threadId: args.threadId,
+				},
+				{
+					prompt: args.prompt,
+					tools: {
+						escalateConversation,
+						resolveConversation,
+					},
+				},
+			)
+		} else {
+			await saveMessage(ctx, components.agent, {
 				threadId: args.threadId,
-			},
-			{
 				prompt: args.prompt,
-			},
-		)
+			})
+		}
 	},
 })
 
